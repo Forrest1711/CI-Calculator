00a728d22b62683fce705412b353b96c481aeede
Forrest <forrest.90h@gmail.com>
2021-06-06 21:21:18 -0700

    added tests for negative values in the functions

--- test_calculator.py
+++ test_calculator.py
@@ -16,3 +16,15 @@ class TestCalculatorApp:
 
     def test_divide(self):
         assert 12 == calculator.divide(144, 12)
+
+    def test_add_neg():
+        assert 3 == calculator.add(12, -9)
+    
+    def test_subtract_neg():
+        assert 21 == calculator.subtract(12, -9)
+    
+    def test_multiply_neg():
+        assert -21 == calculator.multiply(-3,7)
+
+    def test_divide_neg():
+        assert -5 == calculator.divide(-70, 14)
